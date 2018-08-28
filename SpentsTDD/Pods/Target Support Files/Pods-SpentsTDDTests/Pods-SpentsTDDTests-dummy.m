9ad459d6e12e3236c877d20929334d19c71a4ce6
Unknown <matheuspgarcia@gmail.com>
2018-08-28 13:34:53 -0300

    Initiating project

---
+++ SpentsTDD/Pods/Target Support Files/Pods-SpentsTDDTests/Pods-SpentsTDDTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SpentsTDDTests : NSObject
+@end
+@implementation PodsDummy_Pods_SpentsTDDTests
+@end
---
+++ SpentsTDD/SpentsTDD/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SpentsTDD
+//
+//  Created by Matheus Garcia on 28/08/18.
+//  Copyright © 2018 Matheus Garcia. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
